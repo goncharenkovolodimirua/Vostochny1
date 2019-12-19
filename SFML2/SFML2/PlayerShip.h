b36c83a73c227fca0dc2d93df7f52ecd54300b31
Funticke <gimnasia30.petrosavodsk@gmail.com>
2019-12-19 06:05:52 +0300

    Fix. Changed list type in PlayerShipObject

--- SFML2/SFML2/PlayerShip.h
+++ SFML2/SFML2/PlayerShip.h
@@ -1,6 +1,7 @@
 #ifndef __PLAYER_SHIP__
 #define __PLAYER_SHIP__
 #include "SFML_GraphicalGameObject.h"
+#include "PhysicalGameObject.h"
 #include <list>
 class PlayerShip :
     public SFML_GraphicalGameObject
@@ -11,7 +12,7 @@ private:
     int controlHeightMin;
     int controlHeightMax;
 
-	std::list<SFML_GameObject*>* bulletsList;
+	std::list<PhysicalGameObject*>* bulletsList;
 
 public:
     PlayerShip(
@@ -28,7 +29,7 @@ public:
         int controlWidthMax,
         int cotrolHeightMin,
         int controlHeightMax,
-        std::list<SFML_GameObject*>* bulletsList
+        std::list<PhysicalGameObject*>* bulletsList
     );
     ~PlayerShip();
         
